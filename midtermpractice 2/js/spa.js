bd9a3a82f98fa4f05585390cf0f23b8c0f828539
Student <jsitbon@qcc.cuny.edu>
2019-10-25 10:00:11 -0400

    webpages added route directory

---
+++ midtermpractice 2/js/spa.js
@@ -0,0 +1,48 @@
+var app = angular.module('spaApp',['ngRoute']);
+
+
+
+
+          app.config(function($routeProvider) {
+            $routeProvider.when('/',
+              {
+                controller:'CustomersController',
+                templateUrl: 'app/views/home.html'
+              })
+
+
+            .when('/works',
+              {
+                controller: 'ExperienceController',
+                templateUrl: 'app/views/works.html'
+              })
+
+            .when('/news',
+              {
+                controller: 'ExperienceController',
+                templateUrl: 'app/views/news.html'
+                })
+            .when('/contact',
+              {
+                controller: 'ExperienceController',
+                templateUrl: 'app/views/contact.html'
+              })
+
+
+
+          });
+
+
+
+
+          app.controller('ExperienceController', function($scope){
+            $scope.customerID = 5;
+          });
+          app.controller('CustomersController', function($scope) {
+            $scope.customers = [
+                {"id": 1, "name":"Ted", "total": 5.996},
+                {"id": 2, "name":"Michelle", "total": 10.994},
+                {"id": 3, "name":"Zed", "total": 10.99},
+                {"id": 4, "name":"Tina", "total": 15.994}
+              ];
+            });
